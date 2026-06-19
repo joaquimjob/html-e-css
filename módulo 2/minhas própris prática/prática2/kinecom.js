d6e1e4041c856f12ad36f074356f850b16704d28
joaquimjob <joaquimjob962@gmail.com>
2026-06-19 15:31:14 +0100

    Initial commit

---
+++ módulo 2/minhas própris prática/prática2/kinecom.js
@@ -0,0 +1,12 @@
+ function main(){
+    let ma = window.document.getElementById("m")
+    ma.document.body.style.
+    background-color `yellow`
+
+
+  }
+
+  window.alert(` Seja bem vindo ao Kinecom a organização do futuro`)
+   var j = prompt(`Escreva o teu nome`)
+  document.write( j , " agradeceriámos")
+  
